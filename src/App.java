dff6de3d81980b5295d22bf731ed861555f9f403
Telo <telo.philippe@telecom-paris.fr>
2024-09-19 18:07:47 +0200

    Test commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, mister and misses and anyone in between!");
+        System.out.println("Hello, misses and mister and anyone in between!");
     }
 }
