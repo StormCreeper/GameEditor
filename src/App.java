8baefed6245fdadeb71456d0d4750a6cc4f6f3e7
enzo.gelas <enzo.gelas@telecom-paris.fr>
2024-09-19 18:10:02 +0200

    Test commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        ArrayList<String> s = new ArrayList<String>();
+        s.add("Hello");
+        s.add("world");
+        System.out.println(s);        
     }
 }
