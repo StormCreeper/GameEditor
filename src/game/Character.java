a93c30a19f621ef2631954cc6824b65dbbb632fa
Telo <telo.philippe@telecom-paris.fr>
2024-09-25 11:07:58 +0200

    Started character class

--- src/game/Character.java
+++ src/game/Character.java
@@ -1,5 +1,36 @@
 package game;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 public class Character {
+    
+    private float x;
+    private float y;
+
+    private float velX;
+    private float velY;
+
+    private BufferedImage image;
+
+    public Character(String imagePath) {
+        try {
+            image = ImageIO.read(getClass().getResource(imagePath));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        x = 0;
+        y = 0;
+        velX = 0;
+        velY = 0;
+    }
+
+    public void drawSelf(Graphics2D g) {
+        g.drawImage(image, (int)x, (int)y, 100, 100, null);
+    }
+    
 
 }
