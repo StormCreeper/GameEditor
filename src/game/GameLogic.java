8523ac41d230a8bcbf847c693d1bb015b0405761
Telo <telo.philippe@telecom-paris.fr>
2024-09-25 11:07:50 +0200

    Add a game logic component

---
+++ src/game/GameLogic.java
@@ -0,0 +1,26 @@
+package game;
+
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
+
+public class GameLogic {
+    public GameLogic() {
+
+    }
+
+    public void update(float currentTime, float deltaTime) {
+
+    }
+
+    public void draw(Graphics2D g) {
+        
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+    }
+}
