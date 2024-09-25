d5a17d78cc323273f0ed0948f9c2cc83aee821ad
Telo <telo.philippe@telecom-paris.fr>
2024-09-25 11:24:22 +0200

    Added draw example

--- src/game/GameLogic.java
+++ src/game/GameLogic.java
@@ -1,5 +1,6 @@
 package game;
 
+import game.Character;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import javax.swing.JPanel;
@@ -7,6 +8,7 @@ import javax.swing.JPanel;
 public class GameLogic {
 
 	private Tileset tileset;
+    private Character character;
 
     private JPanel parent;
 
@@ -18,19 +20,21 @@ public class GameLogic {
     }
 
     public void update(float currentTime, float deltaTime) {
-
     }
 
     public void draw(Graphics2D g) {
         g.clearRect(0, 0, parent.getWidth(), parent.getHeight());
 
-        for(int i=0; i<7; i++) {
-            g.drawImage(tileset.getTextureArray().get(i), i*100, 0, 90, 90, null);
-        }
+        // Exemple drawing
+
+        for(int j=0; j<7; j++) {
+            for(int i=0; i<7; i++) {
+                g.drawImage(tileset.getTextureArray().get((i+j)%7), i*100, j*100, 90, 90, null);
+            }
+        }   
     }
 
     public void keyPressed(KeyEvent e) {
-
     }
 
     public void keyReleased(KeyEvent e) {
