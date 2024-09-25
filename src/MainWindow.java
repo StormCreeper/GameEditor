a828daa83a10c69fd0e48f5ff3cd212624a07124
Telo <telo.philippe@telecom-paris.fr>
2024-09-25 10:05:25 +0200

    Added game loop

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,5 +1,6 @@
-import javax.swing.JFrame;
+import game.InGamePanel;
 import java.awt.Dimension;
+import javax.swing.JFrame;
 
 public class MainWindow  extends JFrame{
 
@@ -8,6 +9,8 @@ public class MainWindow  extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         setPreferredSize(new Dimension(1000, 800));
+
+        setContentPane(new InGamePanel());
         
         pack();
         setVisible(true);
--- src/game/InGamePanel.java
+++ src/game/InGamePanel.java
@@ -1,8 +1,58 @@
 package game;
 
-public class InGamePanel {
-    public Ima
-    public static void main() {
-        
-    }
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class InGamePanel extends JPanel implements Runnable{
+
+    private long startingNanoTime;
+	private long lastNanoTime;
+	
+	private float currentTime;
+	private float deltaTime;
+	
+	private float pos = 0;
+	
+	Thread thread;
+
+	public InGamePanel() {
+	}
+	
+    @Override
+	public void addNotify() {
+		super.addNotify();
+		if(thread == null) {
+			thread = new Thread(this);
+			thread.start();
+		}
+	}
+	
+	@Override
+	public void run() {
+		startingNanoTime = System.nanoTime();
+		while(true) {
+			long currentNanoTime = System.nanoTime() - startingNanoTime;
+			deltaTime = (currentNanoTime - lastNanoTime) * 1e-9f;
+			currentTime = currentNanoTime * 1e-9f;
+			updateGame();
+			repaint();
+		}
+	}
+	
+	private void updateGame() {
+		pos = 110 + 100 * (float)Math.sin((double)currentTime);
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		g.clearRect(0, 0, getWidth(), getHeight());
+		
+		g.setColor(Color.red);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+		g.setColor(Color.yellow);
+		g.fillRect((int) pos, 10, 100, 100);
+	}
+
 }
