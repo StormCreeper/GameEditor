5ea97f841a5108057e699b4c31a4ea4015c44225
enzo.gelas <enzo.gelas@telecom-paris.fr>
2024-09-25 10:21:34 +0200

    Tilemap

--- src/game/Tilemap.java
+++ src/game/Tilemap.java
@@ -5,5 +5,10 @@ public class Tilemap {
     
     public Tilemap(int width, int height){
         tileMap = new Tile[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+               tileMap[i][j] = new Tile();
+            }         
+        }
     }
 }
