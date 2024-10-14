83ecc046630068db1b271bf233ade4ac4afefde5
Telo Philippe <telo.philippe@gmail.com>
2024-10-14 20:11:57 +0200

    Added a way to represent a tile with an integer

--- src/game/Tile.java
+++ src/game/Tile.java
@@ -12,6 +12,7 @@ public class Tile {
     };
 
     private Type type;
+    private int[] layersTexturesID = {0, 0};
 
     private ArrayList<Integer> baseTextures = new ArrayList<>(); // The texture ids that are used to create the first layer of the tile
 
@@ -24,6 +25,13 @@ public class Tile {
 
     public Tile(Type type) {
         this.type = type;
+    }
+    
+    // 2 bits for type, 15 bits for collision layer, 15 bits for decoration layer
+    public Tile(int repr) {
+        this.type = intToType(repr & 0b11);
+        this.layersTexturesID[0] = (repr >> 2) & 0b111111111111111;
+        this.layersTexturesID[1] = (repr >> 17) & 0b111111111111111;
     }
 
     public void setType(Type type) {
@@ -51,11 +59,26 @@ public class Tile {
         baseTextures = new ArrayList<>();
     }
 
+    public Type intToType(int i) {
+        return switch (i) {
+            case 0 -> Type.ground;
+            case 1 -> Type.water;
+            case 2 -> Type.lava;
+            default -> Type.ground;
+        };
+    }
+
     public int typeToInt() {
         return switch (type) {
             case ground -> 0;
             case water -> 1;
             case lava -> 2;
         };
+    }
+
+    // Encode the tile in an integer
+    // 2 bits for type, 15 bits for collision layer, 15 bits for decoration layer
+    public int repr() {
+        return typeToInt() | layersTexturesID[0] << 2 | layersTexturesID[1] << 17;
     }
 }
